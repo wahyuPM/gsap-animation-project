b73cb2d1b8fbf906ce4034c40df6aee628eb8c36
wahyuPM <wahyupratama191@gmail.com>
2022-08-14 14:40:25 +0800

    first commit

---
+++ 1. Home GSAP/app.js
@@ -0,0 +1,6 @@
+const navLinks = document.querySelectorAll('nav a');
+const imgsNav = document.querySelectorAll('nav img');
+const title = document.querySelector('h1');
+const verticalLine = document.querySelector('.middle-line');
+const grapesLogo = document.querySelector('.home-content img');
+const homebtn = document.querySelector('.home-content button');
