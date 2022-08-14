64f2faa21134250ea60ef5b795340aa9e4dc4aa6
wahyuPM <wahyupratama191@gmail.com>
2022-08-14 15:01:26 +0800

    add : reveal animation

--- 1. Home GSAP/app.js
+++ 1. Home GSAP/app.js
@@ -4,3 +4,17 @@ const title = document.querySelector('h1');
 const verticalLine = document.querySelector('.middle-line');
 const grapesLogo = document.querySelector('.home-content img');
 const homebtn = document.querySelector('.home-content button');
+
+const revealAnim = () => {
+    const TLFADE = gsap.timeline()
+
+    TLFADE
+        .from(title, { autoAlpha: 0, y: -50, delay: 0.2 })
+        .to(verticalLine, { height: 200 }, '-=0.2')
+        .from(grapesLogo, { autoAlpha: 0, y: -50 }, '-=0.2')
+        .from(homebtn, { autoAlpha: 0, y: -50 }, '-=0.2')
+        .from(navLinks, { autoAlpha: 0, y: -50, duration: 0.4, stagger: 0.1 }, '-=0.2')
+        .from(imgsNav, { autoAlpha: 0, y: -50 })
+}
+
+window.addEventListener('load', revealAnim)
